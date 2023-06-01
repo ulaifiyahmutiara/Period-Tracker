cf89d4253045f90a28626499b4afcf32ab358c8e
steven <stevensvn25@student.uns.ac.id>
2023-06-01 20:42:18 +0700

    msi slh angka

--- inputT.py
+++ inputT.py
@@ -2,14 +2,38 @@
 
 import datetime as dt
 
-tgl = (input(f'Masukkan Tanggal Terakhir Mens Kamu: '))
-bln = (input(f'Masukkan Bulan Terakhir Mens Kamu: '))
-rata = (input(f'Kamu Biasanya Mens Berapa Hari Sih? '))
+# tgl = (input(f'Masukkan Tanggal Terakhir Mens Kamu: '))
+# bln = (input(f'Masukkan Bulan Terakhir Mens Kamu: '))
+# rata = (input(f'Kamu Biasanya Mens Berapa Hari Sih? '))
 
-tanggal = '''
-    {}       {}        {}'''.format(tgl, bln, rata)
+# tanggal = '''
+#     {}       {}        {}'''.format(tgl, bln, rata)
 
-database = open('databasetanggal.csv', 'a')
-database.write(tanggal)
-database.close()
+# database = open('databasetanggal.csv', 'a')
+# database.write(tanggal)
+# database.close()
 
+kira_mens = []
+durasi = 7
+for lamanya in range(1, durasi + 1):
+    kira_mens.append(lamanya)
+    
+kira_folikular = []
+durasi = 4
+for lamanya in range(1, durasi + 1):
+    kira_folikular.append(lamanya)
+
+kira_ovulasi = []
+durasi = 7
+for lamanya in range(1, durasi + 1):
+    kira_ovulasi.append(lamanya)
+
+kira_luteal = []
+durasi = 13
+for lamanya in range(1, durasi + 1):
+    kira_luteal.append(lamanya)
+
+print(kira_folikular)
+print(kira_luteal)
+print(kira_mens)
+print(kira_ovulasi)
