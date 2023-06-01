6cb2b687d986d6e7c793452ee5afe009658c5767
steven <stevensvn25@student.uns.ac.id>
2023-06-01 17:42:03 +0700

    coba2 blm ad isi

---
+++ insight.py
@@ -0,0 +1,24 @@
+#INSIGHT
+
+import numpy as np
+import matplotlib.pyplot as mpl
+
+def olahraga():
+    print(f'')
+
+def makanan():
+    print(f'')
+
+def tidur():
+    print(f'')
+
+def haid():
+    folikular = [1, 2, 3, 4]
+    mens = [5, 6, 7, 8, 9, 10, 11]
+    ovulasi = [12, 13, 14, 15, 16, 17, 18]
+    luteal = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    hormon = [2, 4, 6, 7]
+
+    
+
+
